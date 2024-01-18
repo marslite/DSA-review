6ef6525efddf4e0efdee64aa9f04050f074a2be6
menth0s <marslite@pm.me>
2024-01-17 19:32:26 -0500

    Basic Strings operations

--- introduction.py
+++ introduction.py
@@ -50,6 +50,44 @@ names_and_ages = zip(names,ages);
 #Let's not forget to use list to list the lists of tuples
 print(list(names_and_ages));
 
+#Enumerate
+names_in_order = ["Sam", "Bob", "Ann"];
+for i,name in enumerate(names_in_order):
+    print(i,name);
+
+
 
 
 #Initializing 2D Array
+N=10;
+#The 0 is the number that will be used to intialize the 2d array
+matrix = [ [0 for i in range(0,N)] for j in range(0,N) ]
+print(matrix);
+
+#Basic String Operations
+
+s = 'abcdef'
+print(s[4]);
+
+# Substring extraction
+print(s[1:4])
+
+str_ = 'abcdefghij';
+print(str_[1:8:2]);
+#Here we are saying start from the first elemetn and stop at the last one, the step will be -2 each time.
+print(str_[::-2]); #jhfdb
+
+str1 = 'Good';
+str2 = 'Morning';
+str3 = 'Vietnam';
+print(str1 + " " + str2 + " " + str3 + "!");
+
+# Function to split the string by "_" and then printing the splitted_aray at index 2
+def get_enzyme(ln):
+    splitted = ln.split("_")
+    enzyme_product = splitted[2]
+    print(enzyme_product)
+
+
+line = "lacZ_1013aa_betagalactosidase_escherichiacoli";
+get_enzyme(line);
