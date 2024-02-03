8d33f646beff60a2a2defeb0601621f6c59b0a28
menth0s <marslite@pm.me>
2024-02-02 23:31:52 -0500

    Initializing Dummy node for the queue

--- Week_2/queue.py
+++ Week_2/queue.py
@@ -1,1 +1,31 @@
 #Getting started with Queue
+
+class Node:
+    def __init__(self,item):
+        self.item = item
+        self.next = None
+
+class Queue:
+    #Create an empty Queue
+    def __init__(self):
+        self._size = 0
+        self.head = Node('Dummy')
+        self.tail = self.head
+
+    #Adds an item to the queue
+    def enqueue(self, item):
+        pass
+    #Removes and returns the least recently added item
+    def dequeue(self):
+        pass
+
+    #Returns a boolean indiciating whether the queue is emtpy or not
+    def isEmpty(self):
+        pass
+    #Returns the numner of itmes in the queue
+    def size(self):
+        pass
+
+    #Returns a list of items in the queue
+    def items(self):
+        pass
