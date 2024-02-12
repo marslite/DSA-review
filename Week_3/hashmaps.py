9cd7ac810efa217a0769b47b2eb590448c404e8f
menth0s <marslite@pm.me>
2024-02-12 18:45:03 -0500

    Non efficient hashmaps

--- Week_3/hashmaps.py
+++ Week_3/hashmaps.py
@@ -1,3 +1,5 @@
+from time import time
+
 class HashMap:
     def __init__(self):
     #Given a key, will return the value associated with that key if it's in 
@@ -19,3 +21,54 @@ class HashMap:
         #Private function to compute an integer hash value for a given key.
         #O(1) runtime
         pass
+
+    def hash_integer(integer_key):
+        #Set M equal to a large prime number. (We could always pick larger)
+        M = 97
+        return integer_key % 97
+    
+    def hash_string(string_key):
+        M = 1001
+        # R is the size of the Alphabet. Because we are working with ASCII 256,
+        #We set R = 256.
+        R = 256
+        hash_value = 0
+        for char in string_key:
+            hash_value = (hash_value * R + int(char)) % M
+        return hash_value
+    
+    
+
+#Properties of a Hash Function
+#Deterministic  - The same key will always produce the same hash value
+#Efficient - It should be fast to compute
+#Uniform - The hash function should distribute  the keys uniformly
+
+# These three functions below DO NOT SATISFY the above conditions
+    
+    #Hash returns the current time in seconds mod 97
+    def non_deterministic_hash(integer_key):
+        M = 97
+        current_time_int = int(time.time())
+        return current_time_int % 97
+    
+    #Hash returns the square of integer key mod M
+    def non_efficient_hash(integer_key):
+        M = 97
+        hash_value = 0
+        for i in range(integer_key):
+            M = 97
+            hash_value = 0
+            for i in range(integer_key):
+                for j in range(integer_key):
+                    hash_value += 1
+        return hash_value % M
+        
+    
+    #Hash that maps every integer key to 1.
+    def non_uniform_hash(integer_key):
+        return 1
+
+    
+
+
