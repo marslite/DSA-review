70d76c38611a9e6e285d634b43fbe24e7a806f33
menth0s <marslite@pm.me>
2024-02-12 18:09:52 -0500

    Week_3 Hashmaps

---
+++ Week_3/hashmaps.py
@@ -0,0 +1,21 @@
+class HashMap:
+    def __init__(self):
+    #Given a key, will return the value associated with that key if it's in 
+    #the Table. This function will return None if the key is not in the hash map.
+    #O(1) runtime
+        pass
+
+    def put(self, key, value):
+    #Inserts the key-value pair into the hashmap
+    #If the key already exists it will overwrite the previous valu with the new value
+    #O(1) Runtime
+        pass
+
+    def containts(self, key):
+        #Returns true if it contains the key
+        pass
+
+    def _hash(self, key):
+        #Private function to compute an integer hash value for a given key.
+        #O(1) runtime
+        pass
