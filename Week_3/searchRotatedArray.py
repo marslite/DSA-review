8b815d8cc756c9a9fd03aaf92f8f1d3208edcebc
menth0s <marslite@pm.me>
2024-02-16 01:17:10 -0500

    Updated for duplicates and Runtime and Space complexity

--- Week_3/searchRotatedArray.py
+++ Week_3/searchRotatedArray.py
@@ -4,12 +4,14 @@
 
 
 def searchRotatedArray(arr,target):
+    #Runtime complexity: O(log n)
+    #Space complexity: O(1)
     lo = 0
     hi = len(arr) - 1
     while(lo <= hi):
         mid = (lo + hi )// 2
         if arr[mid] == target:
-            return "Target found at position " +  str(mid)
+            return "Target:  " + str(target) +  "  found at position " +  str(mid)
         #case1 arr[lo] > arr[mid] -> we have pivot in between
         if arr[lo] > arr[mid]:
             if target >= arr[lo] or target <= arr[mid]:
@@ -17,19 +19,50 @@ def searchRotatedArray(arr,target):
             else:
                 lo = mid + 1
         else:
-            if target > mid:
+            if target > arr[mid]:
                 lo = mid +1
             else:
                 hi = mid -1
-    return ("Nothing was found with the target")
+    return "Nothing was found with the target: " + str(target)
 
         #case2 arr[lo] < arr[mid] -> no pivot, just perform normal binary search
 
+
+def searchRotatedArraywithDuplicates(arr,target):
+    #Runtime complexity: O(log n)
+    #Space complexity: O(1)
+    lo = 0
+    hi = len(arr) - 1
+    while(lo <= hi):
+        mid = (lo + hi )// 2
+        if arr[mid] == target:
+            return "Target:  " + str(target) +  "  found at position " +  str(mid)
+        
+        while(lo != mid and arr[lo] == arr[mid] ):
+            lo +=1
+        #case1 arr[lo] > arr[mid] -> we have pivot in between
+        if arr[lo] > arr[mid]:
+            if target >= arr[lo] or target <= arr[mid]:
+                hi = mid -1
+            else:
+                lo = mid + 1
+        else:
+            if target > arr[mid]:
+                lo = mid +1
+            else:
+                hi = mid -1
+    return "Nothing was found with the target: " + str(target)
 
 
 
 if __name__ == '__main__':
     arr = [1,3,5,7,9,11,13,15,17,19]
     rot_arr = [13,15,17,19,1,3,5,7,9,11]
+    rot_dup = [11,11,11,11,11,11,11,11,11]
 
-    print(searchRotatedArray(rot_arr,15));
+    # print(searchRotatedArray(rot_arr,15))
+
+    for i in range(0,20):
+        # print(searchRotatedArray(rot_arr,i))
+        # print(searchRotatedArray(arr,i))
+        print(searchRotatedArraywithDuplicates(rot_dup,i))
