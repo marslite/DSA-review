f13a858d67758748bb8d753748a22d36764b7a80
menth0s <marslite@pm.me>
2024-04-08 23:37:43 -0700

    Initializing Trie

---
+++ Week_5/trie.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self):
+        self.childredn = {}
+        self.isWord = False
+
+
+
+class Trie:
+
+    def __init__(self):
+        'Initialize the Trie data structure'
+        pass
+
+
+    def insert(self, word: str) -> None:
+        'It will insert the word by each letter into the trie '
+        pass
+
+
+    def search(self, word:str) -> bool:
+        'Returns if the word we are looking for is our Trie'
+        pass
+
+    
+    def startsWith(self, prefix: str) -> bool:
+        'Returns if there is any word in the trie that starts with the given prefix'
+        pass
