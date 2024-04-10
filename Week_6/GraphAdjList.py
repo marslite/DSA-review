98aeb4d2f8ae420fedcde0cd34c20800da7abe72
menth0s <marslite@pm.me>
2024-04-10 04:24:13 -0700

    Working with Adjancy List

---
+++ Week_6/GraphAdjList.py
@@ -0,0 +1,30 @@
+if __name__ == "__main__":
+    
+    
+    love_connections = [("Lysander", "Helena"), ("Hermia", "Lysander"),
+                        ("Demetrius", "Hermia"), ("Helena", "Demetrius"), ("Titania", "Oberon"),
+                        ("Oberon","Titania"), ("Puck", None), ("Lysander", "Puck")]
+
+#Lysander -> Helena
+#Hermia -> Lysander
+#Demetrius -> Hermia
+#Helena -> Demetrius
+#Titania -> Oberon
+#Oberon -> Titania
+#Puck
+
+adj_list =  {}
+
+for source,target in love_connections:
+    print("source", source, "target", target)
+    if source in adj_list:
+        print("Reached with", source )
+        # print("if source", source,"in adj_list", adj_list)
+        # print("adj_list[source].append(target)", adj_list[source], target)
+
+        adj_list[source].append(target)
+    else:
+        # print("adj_list[source]", adj_list[source], " = target",target )
+        adj_list[source] = [target]
+
+print(adj_list)
