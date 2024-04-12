8343593c112677b19588c8af93d116e1a18d89e0
menth0s <marslite@pm.me>
2024-04-12 03:48:23 -0700

    Implemented Detect_cycle

--- Week_6/detect_cycle.py
+++ Week_6/detect_cycle.py
@@ -1,33 +1,38 @@
 from graphHelpers import buildAdjList
 
 
-
-
 def detectCycle(adj_list):
+    #O(n)
     visited = set()
 
+    #O(N^2) runtime or O(E + V)
+    #o(N) Space complexity
     for key in adj_list.keys():
+        #O(n)
         if key not in visited:
+            #O(n)
             if (DFS(key, set(), visited, adj_list)):
                 return True
             
-            visited.add(key)
+            # visited.add(key)
     return False
 
 
 def DFS(cur_node, cur_path, visited, adj_list):
-    if cur_node in cur_path:
-        return True
+    # if cur_node in cur_path:
+    #     return True
     
     cur_path.add(cur_node)
     visited.add(cur_node)
 
-    for neighbor in adj_list:
+    #N
+    for neighbor in adj_list[cur_node]:
         if cur_node == neighbor:
             continue
         if neighbor in cur_path:
             return True 
         
+        #O(n)
         if neighbor not in visited and DFS(neighbor, cur_path, visited,adj_list):
             return True
 
@@ -41,9 +46,9 @@ def DFS(cur_node, cur_path, visited, adj_list):
 if __name__ == '__main__':
     love_conntections = [("Hermia", "Lysander"), ("Demetrius", "Lysander"), ("Oberon", "Titania"),
                         ("Puck", "Puck"), ("Lysander", "Puck"), ("Helena", "Titania"), ("Hermia", "Puck"), ("Puck", "Helena"),
-                        ("Titania", "Titania")]
+                        ("Titania", "Titania"), ("Helena","Hermia")]
     
 
     adj_list = buildAdjList(love_conntections)
 
-    print(detectCycle(love_conntections))
+    print(detectCycle(adj_list))
