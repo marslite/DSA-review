193229e396ae667523a4c7a06491e243baf5a946
menth0s <marslite@pm.me>
2024-06-03 19:31:08 -0700

    Implemented search duplicates with BS

--- Week_8/searchRotatedArray.py
+++ Week_8/searchRotatedArray.py
@@ -41,6 +41,37 @@ def searchRoratedArray(arr,target):
     return None
 
 
+def searchRoratedArrayWithDuplicates(arr,target):
+    lo = 0
+    hi = len(arr) -1
+    while(lo <= hi):
+        mid = (lo + hi) //2
+        if arr[mid] == target:
+            return mid
+        
+        while(lo != mid and arr[mid] == arr[lo]):
+            lo += 1
+
+        if  arr[lo] > arr[mid]:
+            if target >= arr[lo] or target <= arr[mid]:
+                hi = mid -1
+            else:
+                lo = mid +1
+        #Case 2 arr[lo] < arr[mid] -> no pivot, just perform normal binary search
+        else:
+            if arr[mid] > target:
+                lo -=1
+            else:
+                hi += 1
+            #Both implementations work
+            # if target > mid:
+            #     lo = mid + 1
+            # else:
+            #     hi = mid -1
+    return None
+
+
+
 
 
 
@@ -50,5 +81,7 @@ if __name__ == "__main__":
     print("Binary Search ~ Rotated Array")
     arr = [1,3,5,7,9,11,13,15,17,19]
     rot_arr = [13,15,17,19,1,3,5,7,9,11]
+    dupl_arr = [11,11,11,2,11,11]
     print(standardBinarySearch(arr,11))
     print(searchRoratedArray(rot_arr,1))
+    print(searchRoratedArrayWithDuplicates(dupl_arr,2))
