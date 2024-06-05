380cd660885b540d59a3e115d46d82756552f770
menth0s <marslite@pm.me>
2024-06-04 19:03:58 -0700

    Implemented Selection Sort

--- Week_8/selectionSort.py
+++ Week_8/selectionSort.py
@@ -1,2 +1,22 @@
+def selectionSort(arr):
+    #Runtime: O(n) * O(n) = O(n^2)
+    for i in range(len(arr)):
+        min_number = arr[i]
+        swap_idx = i
+        for j in range(i+1,len(arr)): #O(n)
+            if arr[j] < min_number:
+                min_number = arr[j]
+                swap_idx = j
+        arr[i], arr[swap_idx] = arr[swap_idx], arr[i] #Swampping is Contant Time
+    return arr
+
+
+
+
+
+
+
 if __name__ == "__main__":
     print("Selection Sort")
+    arr = [10,6,8,4,5,1,3,7,2,2,2,2,3]
+    print(selectionSort(arr))
